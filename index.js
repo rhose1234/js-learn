68adfc419417a980a72c0a5fc7dd112beb67a95d
rhose1234 <initial push>
2024-10-19 13:08:53 +0100

    first push

---
+++ index.js
@@ -0,0 +1,22 @@
+let firstname = "rose";
+let secondName = "akorede";
+
+console.log( firstname, secondName);
+
+let arr = [1,2,3,4,5,6];
+
+arr.push(7,8);
+
+console.log(arr);
+
+const  person = {
+    name : "rose",
+    age: 21,
+    level: "graduate",
+    church: "kfc"
+
+};
+
+person.name = "Marvelous" ;
+person.age = 22;
+console.log(person);
